373b2015e78482f8ae9753f82e56525261634b0f
Karim Gonzalez <karim.gonzalez@bosonit.com>
2023-01-09 14:39:50 +0100

    update filtro

--- src/main/java/org/example/ejerciciocontroller/EjercicioControllerApplication.java
+++ src/main/java/org/example/ejerciciocontroller/EjercicioControllerApplication.java
@@ -3,7 +3,9 @@ package org.example.ejerciciocontroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class EjercicioControllerApplication {
 
--- src/main/java/org/example/ejerciciocontroller/controller.java
+++ src/main/java/org/example/ejerciciocontroller/controller.java
@@ -21,15 +21,14 @@ public class controller {
         ControllerObject controllerObject = new ControllerObject();
 
         //Creo listas con los valores de headers y querys
-
         List<Header> headersList = createHeadersList(headers);
         List<Query> querysList = createQuerysList(querys);
-
 
         // Asigna los valores a los campos de controllerObject
         controllerObject.setPaths(extractPaths(request));
         controllerObject.setQuerys(querysList);
         controllerObject.setHeaders(headersList);
+
         //Esto extrae la IP remota, si se muestra 0.0.0.0.0.0.1 es porque estás intentando acceder
         // a tu local machine pero si alguien externo lo intenta en la maquina funcionaría mostrando la IP
         //request.getRemoteAddr() o getLocalAddr() para el server donde corre la app
@@ -43,9 +42,11 @@ public class controller {
         String path = request.getRequestURI().substring(request.getContextPath().length());
         return Arrays.asList(path.split("/"));
     }
-
+// Creamos una lista X donde con un for añadimos los name - values de los headers que le pasamos desde los request
     private List<Header> createHeadersList(Map<String, String> headersMap) {
+
         List<Header> headersList = new ArrayList<>();
+
         for (Map.Entry<String, String> entry : headersMap.entrySet()) {
             String name = entry.getKey();
             String value = entry.getValue();
@@ -53,7 +54,8 @@ public class controller {
         }
         return headersList;
     }
-
+    // Creamos una lista X donde con un for añadimos los name - values almacenados en un mapa
+    // de los querys que le pasamos desde los request
     private List<Query> createQuerysList(Map<String, String> querysMap) {
         List<Query> querysList = new ArrayList<>();
         for (Map.Entry<String, String> entry : querysMap.entrySet()) {
